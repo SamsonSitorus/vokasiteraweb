fdbbf44fa74185caff0b791f4ed2943412a066d1
SamsonSitorus <149450441+SamsonSitorus@users.noreply.github.com>
2025-05-15 23:53:53 +0700

    y

--- resources/views/pages/Mahasiswa/Pengumuman/show.blade.php
+++ resources/views/pages/Mahasiswa/Pengumuman/show.blade.php
@@ -45,8 +45,8 @@
 
                         @if ($pengumuman->file)
                         <div class="text-center mt-5">
-                            <a href="{{ asset('storage/' . $pengumuman->file) }}" class="btn btn-success btn-lg" download>
-                                <i class="fas fa-file-download"></i> Download File
+                             <a href="{{ asset('storage/' . $pengumuman->file) }}" class="btn btn-primary btn-sm" target="_blank">
+                                <i class="fas fa-file"></i> Lihat File
                             </a>
                         </div>
                         @endif
