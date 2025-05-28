b8a125085bce8f4602f55f3fd578f8ca821d9ef1
SamsonSitorus <149450441+SamsonSitorus@users.noreply.github.com>
2025-05-28 14:02:21 +0700

    Update penguji_tugas_Controller.php

--- app/Http/Controllers/penguji_tugas_Controller.php
+++ app/Http/Controllers/penguji_tugas_Controller.php
@@ -14,15 +14,15 @@ class penguji_tugas_Controller extends Controller
        $role_ids = [2,4];
        $prodi_ids = DosenRole::where('user_id', $user_id)
                           ->where('status', 'Aktif')
-                          ->where('role_id', $role_ids)
+                          ->whereIn('role_id', $role_ids)
                           ->pluck('prodi_id');
         $TM_ids = DosenRole::where('user_id', $user_id)
                             ->where('status', 'Aktif')
-                            ->where('role_id', $role_ids)
+                            ->whereIn('role_id', $role_ids)
                           ->pluck('TM_id');
         $KPA_ids = DosenRole::where('user_id', $user_id)
                           ->where('status', 'Aktif')
-                          ->where('role_id', $role_ids)
+                          ->whereIn('role_id', $role_ids)
                           ->pluck('KPA_id');
         $prodi_ids = $prodi_ids->unique();
         $TM_ids = $TM_ids->unique();
